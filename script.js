0096324f7e8f2955da64ab4fed1833f0019d41dc
PizzaMyHeart <ian_tan@hotmail.com>
2018-07-24 11:09:30 +0100

    tube fade carousel

--- script.js
+++ script.js
@@ -57,10 +57,21 @@ function getTube() {
 				text.innerHTML = data[i].name + ' - ' + data[i].lineStatuses[0].statusSeverityDescription;
 				if (tube.childNodes[i]) {
 					tube.childNodes[i].innerHTML = data[i].name + ' - ' + data[i].lineStatuses[0].statusSeverityDescription;
+					tube.childNodes[i].classList.add('slide');
 				} else {
 					tube.appendChild(text);
+					tube.childNodes[i].classList.add('slide');
 				};
 			};
+			var current = 0,
+    			slides = document.getElementsByClassName('slide');
+			setInterval(function() {
+	  			for (let i = 0; i < slides.length; i++) {
+	    			slides[i].style.opacity = 0;
+	  			}
+	  			current = (current != slides.length - 1) ? current + 1 : 0;
+	  			slides[current].style.opacity = 1;
+			}, 3000);
 			setTimeout(getTube, 300000);
 		})
 		.catch(function(err) {
